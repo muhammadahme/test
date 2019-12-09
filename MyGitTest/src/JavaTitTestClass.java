9e4469e320ec3727b7b007b21c72250f1281e871
iqbal <iqbal@DESKTOP-CKDMN9M>
2019-12-08 20:17:33 -0500

    my minor change

--- MyGitTest/src/JavaTitTestClass.java
+++ MyGitTest/src/JavaTitTestClass.java
@@ -2,7 +2,8 @@
 public class JavaTitTestClass {
 
 	public static void main(String[] args) {
-		System.out.println(" ma name is iqbal");
+		//System.out.println(" ma name is iqbal");
+		System.out.println(" my friend is added with me");
 
 	}
 
